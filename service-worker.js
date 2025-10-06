4b524cebe1b0db9826833d4e4def68aff87e229e
reneweller123-eng <reneweller123@googlemail.com>
2025-10-06 15:09:18 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "sitzplan-cache-v1";
+const urlsToCache = [
+  "./",
+  "./PWNEU_1.html",
+  "./plan.png",
+  "./manifest.json"
+];
+
+// Installations-Event: Cache aufbauen
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch-Event: Daten aus Cache laden, wenn offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
